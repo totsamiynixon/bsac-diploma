0009d1078daeced3967947b43356b7e110521d34
totsamiynixon_petprojects <totsamiynixon.petprojects@gmail.com>
2018-05-22 16:47:12 +0300

    Created Mockup classes

---
+++ Services.DTO/UserTraining/UserTrainingDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTO.UserTraining
+{
+    public class UserTrainingDTO
+    {
+        public int Id { get; set; }
+
+        public bool IsFinished { get; set; }
+    }
+}
