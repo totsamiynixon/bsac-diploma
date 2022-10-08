1a219137a330664d4d6f01e48e4ebdc78a404a64
Yauheni But-Husaim <totsamiynixon@gmail.com>
2022-10-08 14:27:00 +0200

    cleanup dead code

--- client/Mobile/src/app.vue
+++ client/Mobile/src/app.vue
@@ -12,37 +12,10 @@
       <f7-view url="/panel-left/"></f7-view>
     </f7-panel>
 
-    <!-- Right Panel -->
-    <f7-panel right
-              cover
-              theme-dark>
-      <f7-view url="/panel-right/"></f7-view>
-    </f7-panel>
-
     <!-- Main View -->
     <f7-view id="main-view"
              url="/"
              main></f7-view>
-
-    <!-- Popup -->
-    <f7-popup id="popup">
-      <f7-view>
-        <f7-page>
-          <f7-navbar title="Popup">
-            <f7-nav-right>
-              <f7-link popup-close>Close</f7-link>
-            </f7-nav-right>
-          </f7-navbar>
-          <f7-block>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto.
-            Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis,
-            a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati,
-            quod.</f7-block>
-        </f7-page>
-      </f7-view>
-    </f7-popup>
-
-    <!-- Login Screen -->
-
   </div>
 </template>
 
